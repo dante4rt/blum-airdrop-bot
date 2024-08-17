8a54c85095f195f1e41bfd8ad8236cf928f16e48
Dante4rt <rxmxdhxni@gmail.com>
2024-08-17 16:13:23 +0700

    fix: adjustable header

--- src/display.js
+++ src/display.js
@@ -1,7 +1,7 @@
 function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
-  console.log('=        🌟 Blum Airdrop Bot 🌟         ='.cyan);
+  console.log('=        🌟 Blum Airdrop Bot 🌟        ='.cyan);
   console.log('=     Created by HappyCuanAirdrop      ='.cyan);
   console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
   console.log('========================================'.cyan);
